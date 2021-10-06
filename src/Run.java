ff4b45e1ab5d9789ac8960ad3794be8d05b41c72
Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2021-10-07 01:39:59 +0200

    src/Run.java: cosmetics

--- src/Run.java
+++ src/Run.java
@@ -3,6 +3,8 @@ import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 
+import org.apache.xerces.impl.dtd.models.CMStateSet;
+
 public class Run
 {
   public static void main(String[] args) throws Exception
@@ -14,7 +16,7 @@ public class Run
     String xsd = args.length > 0 ? args[0] : xsds[0];
     String xml = args.length > 1 ? args[1] : xmls[0];
 
-    System.out.println(org.apache.xerces.impl.dtd.models.CMStateSet.class.getResource("CMStateSet.class"));
+    System.out.println(CMStateSet.class.getResource("CMStateSet.class"));
 
     SchemaFactory factory = SchemaFactory.newInstance(xsdVersions[1]);
     Schema schema = factory.newSchema(new StreamSource(xsd));
