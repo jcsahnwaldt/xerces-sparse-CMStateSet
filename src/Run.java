a3c3963b4528ff0a7f20c17918b83ed4789fce92
Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2023-02-16 03:24:13 +0100

    src/Run.java

--- src/Run.java
+++ src/Run.java
@@ -1,13 +1,24 @@
+import static java.lang.Math.pow;
+
 import java.io.File;
 import java.net.URI;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import javax.xml.XMLConstants;
+import javax.xml.namespace.NamespaceContext;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathException;
+import javax.xml.xpath.XPathFactory;
 
 import org.apache.xerces.impl.dtd.models.CMStateSet;
+import org.xml.sax.InputSource;
 
 public class Run
 {
@@ -34,7 +45,55 @@ public class Run
       long nanos = System.nanoTime();
       validator.validate(new StreamSource(xml));
       nanos = System.nanoTime() - nanos;
-      System.out.println(nanos / 1E6 + " millis");
+      printPerformance(xsd, nanos);
     }
+  }
+
+  private static void printPerformance(String xsd, long nanos) throws XPathException {
+    System.out.println(nanos / 1E6 + " millis");
+    if (xsd.equals("test-choice.xsd")) {
+      int max = getInteger(xsd, "/xs:schema/xs:element/xs:complexType/xs:choice/@maxOccurs");
+      System.out.println("choice maxOccurs: "+max);
+      System.out.println("ns/choiceMax^2: "+nanos/pow(max,2));
+    }
+    else if (xsd.equals("test-choice-assert.xsd")) {
+      int max = getInteger(xsd, "/xs:schema/xs:element/xs:complexType/xs:assert/@test");
+      System.out.println("assert max count: " + max);
+    }
+    else if (xsd.equals("test-sequence.xsd")) {
+      int sequenceMax = getInteger(xsd, "/xs:schema/xs:element/xs:complexType/xs:sequence/@maxOccurs");
+      int elementMax = getInteger(xsd, "/xs:schema/xs:element/xs:complexType/xs:sequence/xs:element[@name='b']/@maxOccurs");
+      System.out.println("sequence maxOccurs: " + sequenceMax);
+      System.out.println("element maxOccurs: " + elementMax);
+      System.out.println("ns/(sequenceMax^2*elementMax^2): "+nanos/pow(sequenceMax,2)/pow(elementMax,2));
+      System.out.println("ns/(sequenceMax^3*elementMax^3): "+nanos/pow(sequenceMax,3)/pow(elementMax,3));
+      System.out.println("ns/(sequenceMax^4*elementMax^2): "+nanos/pow(sequenceMax,4)/pow(elementMax,2));
+    }
+  }
+
+  private static int getInteger(String xsd, String expr) throws XPathException {
+    XPath xpath = XPathFactory.newInstance().newXPath();
+    xpath.setNamespaceContext(new XsdNamespaceContext());
+    String value = xpath.evaluateExpression(expr, new InputSource(xsd), String.class);
+    Matcher matcher = Pattern.compile("\\d+").matcher(value);
+    matcher.find();
+    return Integer.parseInt(matcher.group());
+  }
+}
+
+class XsdNamespaceContext implements NamespaceContext {
+  @Override
+  public String getNamespaceURI(String prefix) {
+    return XMLConstants.W3C_XML_SCHEMA_NS_URI;
+  }
+
+  @Override
+  public String getPrefix(String namespaceURI) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public Iterator<String> getPrefixes(String namespaceURI) {
+    throw new UnsupportedOperationException();
   }
 }
